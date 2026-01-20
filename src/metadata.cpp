d2ac833bcc030688de3fad8b840e5223d64e8884
Squirrel <squirrelhomebrew@gmail.com>
2026-01-19 20:37:02 -0800

    ddd

---
+++ src/metadata.cpp
@@ -0,0 +1,47 @@
+#include "metadata.h"
+#include <taglib/fileref.h>
+#include <taglib/tag.h>
+#include <iostream>
+
+void readMetadata(const std::string &file)
+{
+    TagLib::FileRef f(file.c_str());
+    if (f.isNull() || !f.tag())
+        return;
+
+    auto *tag = f.tag();
+    std::cout
+        << "Title:  " << tag->title() << "\n"
+        << "Artist: " << tag->artist() << "\n"
+        << "Album:  " << tag->album() << "\n"
+        << "Year:   " << tag->year() << "\n"
+        << "Track:  " << tag->track() << "\n";
+}
+
+void writeMetadata(
+    const std::string &file,
+    const std::string &title,
+    const std::string &artist,
+    const std::string &album,
+    int year,
+    int track)
+{
+    TagLib::FileRef f(file.c_str());
+    if (f.isNull() || !f.tag())
+        return;
+
+    auto *tag = f.tag();
+
+    if (!title.empty())
+        tag->setTitle(title);
+    if (!artist.empty())
+        tag->setArtist(artist);
+    if (!album.empty())
+        tag->setAlbum(album);
+    if (year)
+        tag->setYear(year);
+    if (track)
+        tag->setTrack(track);
+
+    f.save();
+}
